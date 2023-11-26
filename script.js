88bdcb499d4e87a27d61adb6b789cace5ee3154c
Chandan Gupta <112531619+Chandujixyz@users.noreply.github.com>
2023-11-26 08:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ while(i<=str.length-1){
 }
 
 // Do not change the code below
-// const str = prompt("Enter String.");
-// const size = prompt("Enter Chunk Size.");
-// alert(stringChop(str, size));
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
+alert(stringChop(str, size));
